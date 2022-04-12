bc3b12993baf9d3712c95c6487de082bdb7c305b
Phil Tegg <phil.tegg@gmail.com>
2022-04-12 21:09:56 +0100

    Added key security for Skill Dev.

--- wol-api.py
+++ wol-api.py
@@ -42,4 +42,4 @@ def wol():
 
 # Run local server
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', ssl_context=('certificate.pem', 'private-key.pem'), port=5000)
